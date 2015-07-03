e1bb4dc50577a35240c8fa901364bbaae2f5e08b
Mikhail Dubov <mdubov@gmail.com>
2015-07-04 00:45:41 +0100

    Fix lists

--- app/models/user.js
+++ app/models/user.js
@@ -2,7 +2,6 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   email: DS.attr('string'),
-  mailchimpLists: DS.hasMany('mailchimp-list', { async: true }),
-  availableMailchimpLists: DS.attr(),
+  lists: DS.attr(),
   selectedMailchimpListId: DS.attr('string'),
 });
